3cb3365363f28c841d7f16a0581e5faf9955637c
delaya73 <delaya73@gmail.com>
2024-06-28 15:15:14 +0500

    libmp: edit

--- sys/src/libmp/port/mpadd.c
+++ sys/src/libmp/port/mpadd.c
@@ -9,6 +9,8 @@ mpmagadd(mpint *b1, mpint *b2, mpint *sum)
 	int m, n;
 	mpint *t;
 
+	sum->flags |= (b1->flags | b2->flags) & MPtimesafe;
+
 	// get the sizes right
 	if(b2->top > b1->top){
 		t = b1;
@@ -23,6 +25,7 @@ mpmagadd(mpint *b1, mpint *b2, mpint *sum)
 	}
 	if(m == 0){
 		mpassign(b1, sum);
+		sum->sign = 1;
 		return;
 	}
 	mpbits(sum, (n+1)*Dbits);
@@ -41,6 +44,7 @@ mpadd(mpint *b1, mpint *b2, mpint *sum)
 	int sign;
 
 	if(b1->sign != b2->sign){
+		assert(((b1->flags | b2->flags | sum->flags) & MPtimesafe) == 0);
 		if(b1->sign < 0)
 			mpmagsub(b2, b1, sum);
 		else
--- sys/src/libmp/port/mpextendedgcd.c
+++ sys/src/libmp/port/mpextendedgcd.c
@@ -5,7 +5,7 @@
 
 // extended binary gcd
 //
-// For a anv b it solves, v = gcd(a,b) and finds x and y s.t.
+// For a and b it solves, v = gcd(a,b) and finds x and y s.t.
 // ax + by = v
 //
 // Handbook of Applied Cryptography, Menezes et al, 1997, pg 608.  
@@ -14,6 +14,17 @@ mpextendedgcd(mpint *a, mpint *b, mpint *v, mpint *x, mpint *y)
 {
 	mpint *u, *A, *B, *C, *D;
 	int g;
+
+	if(v == nil){
+		v = mpnew(0);
+		mpextendedgcd(a, b, v, x, y);
+		mpfree(v);
+		return;
+	}
+	assert(x == nil || (x->flags & MPtimesafe) == 0);
+	assert(y == nil || (y->flags & MPtimesafe) == 0);
+	assert((a->flags&b->flags) & MPnorm);
+	assert(((a->flags|b->flags|v->flags) & MPtimesafe) == 0);
 
 	if(a->sign < 0 || b->sign < 0){
 		mpassign(mpzero, v);
@@ -101,6 +112,4 @@ mpextendedgcd(mpint *a, mpint *b, mpint *v, mpint *x, mpint *y)
 	mpfree(u);
 	mpfree(a);
 	mpfree(b);
-
-	return;
 }
